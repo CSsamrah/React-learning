ed8c5225f823076ce749b34ee36905871f30fed2
CSSamrah <samra.fatima.790@gmail.com>
2024-12-26 16:50:05 +0500

    currency converter

---
+++ currencyConverter/src/components/InputBox.jsx
@@ -0,0 +1,51 @@
+import React, { useId } from 'react'
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = 'usd',
+    amountDisable = false,
+    currencyDisable = false,
+    className = ""
+
+}
+
+
+) {
+    const amountId = useId()
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountId} className="text-black/40 mb-2 inline-block">{label}</label>
+                <input
+                    id={amountId}
+                    className='outline-none w-full bg-transparent py-5'
+                    type='number'
+                    placeholder='Amount'
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}//since javascript converts the event into string so its better to wrap it in number since the type of this input field is number
+
+                />
+            </div>
+            <div className='w-1/2 flex flex-wrap justify-end text-right'>
+                <p className='text-black mb-2 w-full'>currency type</p>
+                <select className='rounded-lg px-4  bg-gray-100 cursor-pointer outline-none'
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                    {/* agar performance lani hai loop kay andar to always use keys takay react component degrade na ho  */}
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>{currency}</option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox
