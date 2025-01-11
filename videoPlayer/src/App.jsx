b103f329ef9b7b03c5851ef59d54cb9b2355a87f
CSSamrah <samra.fatima.790@gmail.com>
2025-01-11 18:42:05 +0500

    changes

--- videoPlayer/src/App.jsx
+++ videoPlayer/src/App.jsx
@@ -9,8 +9,9 @@ function App() {
   const videos = {
     deer: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4",
     snail: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4",
-    cat: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4",
     spider: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4",
+    cat: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4",
+    
 
   }
   const videoOptions = Object.keys(videos)
